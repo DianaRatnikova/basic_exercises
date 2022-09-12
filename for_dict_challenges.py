3f683db0186fb3ecc166a1f2854a27868f8e5aaf
DianaRatnikova <Diana_r@bk.ru>
2022-09-13 01:18:44 +0300

    Homework2: for_dict_chalenges: Task 4 and Task5

--- for_dict_challenges.py
+++ for_dict_challenges.py
@@ -1,70 +1,21 @@
-# Задание 1
-# Дан список учеников, нужно посчитать количество повторений каждого имени ученика
-# Пример вывода:
-# Вася: 1
-# Маша: 2
-# Петя: 2
-
-students = [
-    {'first_name': 'Вася'},
-    {'first_name': 'Петя'},
-    {'first_name': 'Маша'},
-    {'first_name': 'Маша'},
-    {'first_name': 'Петя'},
-]
-# ???
-
-
-# Задание 2
-# Дан список учеников, нужно вывести самое часто повторящееся имя
-# Пример вывода:
-# Самое частое имя среди учеников: Маша
-students = [
-    {'first_name': 'Вася'},
-    {'first_name': 'Петя'},
-    {'first_name': 'Маша'},
-    {'first_name': 'Маша'},
-    {'first_name': 'Оля'},
-]
-# ???
-
-
-# Задание 3
-# Есть список учеников в нескольких классах, нужно вывести самое частое имя в каждом классе.
-# Пример вывода:
-# Самое частое имя в классе 1: Вася
-# Самое частое имя в классе 2: Маша
-
-school_students = [
-    [  # это – первый класс
-        {'first_name': 'Вася'},
-        {'first_name': 'Вася'},
-    ],
-    [  # это – второй класс
-        {'first_name': 'Маша'},
-        {'first_name': 'Маша'},
-        {'first_name': 'Оля'},
-    ],[  # это – третий класс
-        {'first_name': 'Женя'},
-        {'first_name': 'Петя'},
-        {'first_name': 'Женя'},
-        {'first_name': 'Саша'},
-    ],
-]
-# ???
-
+# Задача 4, Задача 5
 
 # Задание 4
 # Для каждого класса нужно вывести количество девочек и мальчиков в нём.
 # Пример вывода:
-# Класс 2a: девочки 2, мальчики 0 
+# Класс 2a: девочки 2, мальчики 0
 # Класс 2б: девочки 0, мальчики 2
 
-school = [
-    {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
-    {'class': '2б', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
-    {'class': '2б', 'students': [{'first_name': 'Даша'}, {'first_name': 'Олег'}, {'first_name': 'Маша'}]},
-]
+school = [{'class': '2a',
+           'students': [{'first_name': 'Маша'},
+                        {'first_name': 'Оля'}]},
+          {'class': '2б',
+           'students': [{'first_name': 'Олег'},
+                        {'first_name': 'Миша'}]},
+          {'class': '2б',
+           'students': [{'first_name': 'Даша'},
+                        {'first_name': 'Олег'},
+                        {'first_name': 'Маша'}]}, ]
 is_male = {
     'Олег': True,
     'Маша': False,
@@ -72,24 +23,88 @@ is_male = {
     'Миша': True,
     'Даша': False,
 }
-# ???
 
+
+def num_of_boys_and_girls(school):
+    for school_class in school:
+        num_of_girls = 0
+        num_of_boys = 0
+        num_of_gender_neutral = 0
+        for first_name in school_class['students']:
+            # Вопрос:
+            # Норм ли, что здесь я:
+            # осознанно не проверяю условие
+            # if first_name['first_name'] in is_male:
+            # (не хочу загромождать код и исхожу из того,
+            # что все имена точно есть в массиве с полом )
+            # а если нет - использую исключения
+            try:
+                if is_male[first_name['first_name']]:
+                    num_of_boys += 1
+                else:
+                    num_of_girls += 1
+            except KeyError:
+                num_of_gender_neutral += 1
+        print(f"Класс {school_class['class']}: девочки {num_of_girls}",
+              f"мальчики {num_of_boys}, не определено {num_of_gender_neutral}")
+
+
+print("\n==== Задача 4  =====")
+num_of_boys_and_girls(school)
 
 # Задание 5
-# По информации о учениках разных классов нужно найти класс, в котором больше всего девочек и больше всего мальчиков
+# По информации о учениках разных классов нужно найти класс,
+# в котором больше всего девочек и больше всего мальчиков
 # Пример вывода:
 # Больше всего мальчиков в классе 3c
 # Больше всего девочек в классе 2a
 
-school = [
-    {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
-    {'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
-]
+school = [{'class': '2a',
+           'students': [{'first_name': 'Маша'},
+                        {'first_name': 'Оля'}]},
+          {'class': '3c',
+           'students': [{'first_name': 'Олег'},
+                        {'first_name': 'Миша'}]}, ]
+
 is_male = {
     'Маша': False,
     'Оля': False,
     'Олег': True,
     'Миша': True,
 }
-# ???
 
+
+def find_max_num_of_girls_and_boys(max_num_of, num_of):
+    if num_of > max_num_of:
+        max_num_of = num_of
+    return max_num_of
+
+
+def max_num_of_boys_and_girls(school):
+    max_num_of_girls = 0
+    max_num_of_boys = 0
+    for school_class in school:
+        num_of_girls = 0
+        num_of_boys = 0
+        for first_name in school_class['students']:
+            if is_male[first_name['first_name']]:
+                num_of_boys += 1
+            else:
+                num_of_girls += 1
+
+        school_class['girls'] = num_of_girls
+        school_class['boys'] = num_of_boys
+        max_num_of_girls = find_max_num_of_girls_and_boys(max_num_of_girls,
+                                                          num_of_girls)
+        max_num_of_boys = find_max_num_of_girls_and_boys(max_num_of_boys,
+                                                         num_of_boys)
+
+    for school_class in school:
+        if school_class['girls'] == max_num_of_girls:
+            print(f"Больше всего девочек в классе {school_class['class']}")
+        if school_class['boys'] == max_num_of_boys:
+            print(f"Больше всего мальчиков в классе {school_class['class']}")
+
+
+print("\n==== Задача 5  =====")
+max_num_of_boys_and_girls(school)
