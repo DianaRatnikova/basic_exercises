78889df71f955f24fd499bedbd5a965f0753248c
DianaRatnikova <Diana_r@bk.ru>
2022-09-15 00:48:01 +0300

    Used list comprehension Line38 and added len() for number of words Line11

--- string_challenges.py
+++ string_challenges.py
@@ -8,14 +8,10 @@ word = 'Архангельск'
 
 
 def count_letters(letter_to_find, words):
-    num_of_letters = 0
-    for letter in words.lower():
-        if letter.lower() in letter_to_find.lower():
-            num_of_letters += 1
-    return num_of_letters
+    return words.lower().count(letter_to_find.lower())
 
 
-print(f"Количество букs 'а': {count_letters('а', word)}")
+print(f"Количество букв 'а': {count_letters('а', word)}")
 
 # Вывести количество гласных букв в слове
 word = 'Архангельск'
@@ -27,15 +23,10 @@ for vowel_letter in list_of_vowels:
     num_of_vowels += count_letters(vowel_letter, word)
 print(f"Число гласных: {num_of_vowels}")
 
-
 # Вывести количество слов в предложении
 sentence = 'Мы приехали в гости'
 sentence_list = sentence.split()
-num_of_words = 0
-for word_of_sentence in sentence_list:
-    num_of_words += 1
-print(f"Количество слов в предложении = {num_of_words}\n")
-
+print(f"Количество слов в предложении '{sentence}': {len(sentence_list)}\n")
 
 # Вывести первую букву каждого слова на отдельной строке
 sentence = 'Мы приехали в гости'
@@ -44,8 +35,6 @@ for word_of_sentence in sentence_list:
 
 # Вывести усреднённую длину слова в предложении
 sentence = 'Мы приехали в гости'
-len_avg = 0
-for word_of_sentence in sentence_list:
-    len_avg += len(word_of_sentence)
-    num_of_words += 1
-print(f"\nСредняя длина = {len_avg/num_of_words}")
+sentence_list = [len(word) for word in sentence.split()]
+print(f"\nСредняя длина слова в предложении '{sentence}':"
+      f"{sum(sentence_list)/len(sentence_list)}")
